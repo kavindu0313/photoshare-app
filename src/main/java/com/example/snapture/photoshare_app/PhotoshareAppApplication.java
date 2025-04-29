dfb61838c3a38941fa8dec6fbf184bfbe468e208
Kavindu Senanayake <gihankavindu789@gmail.com>
2025-04-29 13:14:38 +0530

    first commit

---
+++ src/main/java/com/example/snapture/photoshare_app/PhotoshareAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.snapture.photoshare_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoshareAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoshareAppApplication.class, args);
+	}
+
+}
